639b98bc79473acc554c6659aa37e00b0db7c81f
Alexander Linder <alexander-linder@gmx.de>
2020-06-01 02:00:56 +0200

    Add ropeConcat function

--- src/Ropes.hs
+++ src/Ropes.hs
@@ -7,3 +7,7 @@ data Rope
 ropeLength :: Rope -> Int
 ropeLength (Leaf s) = length s
 ropeLength (Inner _ l r) = l + ropeLength r
+
+ropeConcat :: Rope -> Rope -> Rope
+ropeConcat (Inner right inner left) rope = ropeConcat left rope
+ropeConcat (Leaf s) rope = Inner (Leaf s) (ropeLength (Leaf s)) rope
