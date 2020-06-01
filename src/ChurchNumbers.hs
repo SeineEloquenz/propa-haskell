ec71bc93a51127e2d543d7fc4c8bf3b42689b3d2
Alexander Linder <alexander-linder@gmx.de>
2020-06-01 20:57:28 +0200

    Add Church number module

---
+++ src/ChurchNumbers.hs
@@ -0,0 +1,11 @@
+module ChurchNumbers where
+
+type Church t = (t -> t) -> t -> t
+
+int2church :: Integer -> Church t
+int2church 0 = \s z -> z
+int2church n = \s z -> iterate s z !! fromIntegral n
+
+church2int :: Church Integer -> Integer
+-- n = \s z -> s(s(...(z)))
+church2int n = n (+1) 0
