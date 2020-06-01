110f7caeabf371b6122b734683e6ae8de5f5a5b1
Alexander Linder <alexander-linder@gmx.de>
2020-06-01 20:33:32 +0200

    Add ropeInsert function

--- src/Ropes.hs
+++ src/Ropes.hs
@@ -17,3 +17,6 @@ ropeSplitAt i (Inner left len right)
   | i < len = let (leftleft, leftright) = ropeSplitAt i left in (leftleft, ropeConcat leftright right)
   | i > len = let (rightleft, rightright) = ropeSplitAt (i - len) right in (ropeConcat left rightleft, rightright)
   | otherwise = (left, right)
+  
+ropeInsert :: Int -> Rope -> Rope -> Rope
+ropeInsert i a b = let (leftOfI, rightOfI) = ropeSplitAt i b in leftOfI `ropeConcat` a `ropeConcat` rightOfI 
