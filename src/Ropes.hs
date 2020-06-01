ef77a316c79b664f7126888db22de02127d90c4b
Alexander Linder <alexander-linder@gmx.de>
2020-06-01 20:18:02 +0200

    Add ropeSplitAt function

--- src/Ropes.hs
+++ src/Ropes.hs
@@ -10,3 +10,10 @@ ropeLength (Inner _ l r) = l + ropeLength r
 
 ropeConcat :: Rope -> Rope -> Rope
 ropeConcat l r = Inner l (ropeLength l) r
+
+ropeSplitAt :: Int -> Rope -> (Rope, Rope)
+ropeSplitAt i (Leaf s) = (Leaf (take i s), Leaf (drop i s))
+ropeSplitAt i (Inner left len right)
+  | i < len = let (leftleft, leftright) = ropeSplitAt i left in (leftleft, ropeConcat leftright right)
+  | i > len = let (rightleft, rightright) = ropeSplitAt (i - len) right in (ropeConcat left rightleft, rightright)
+  | otherwise = (left, right)
