681f82e7b2c4ba40d6d39ac29fddf52cd40909ea
Alexander Linder <alexander-linder@gmx.de>
2020-06-13 16:18:56 +0200

    Add deriv functions for polynomials

--- src/Polynom.hs
+++ src/Polynom.hs
@@ -10,3 +10,12 @@ eval poly x = foldr ((+).(x*)) 0 poly
 -- these are functionally the same. The latter one shows the horner scheme more clearly while the former is more concise
 evalLambda :: Polynom -> Double -> Double
 evalLambda poly x = foldr (\aN acc -> aN + (x * acc)) 0 poly
+
+deriv :: Polynom -> Polynom
+deriv = derivComposition
+-- Using dollar operator
+derivDollar :: Polynom -> Polynom
+derivDollar poly = zipWith (*) [1..] $ tail poly
+-- Using function composition
+derivComposition :: Polynom -> Polynom
+derivComposition = zipWith (*) [1..] . tail
