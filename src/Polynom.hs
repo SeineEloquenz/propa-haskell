7664ce70963baa8519e88576a5ae8755261829c9
Alexander Linder <alexander-linder@gmx.de>
2020-06-13 16:13:55 +0200

    Add eval functions for polynomials

--- src/Polynom.hs
+++ src/Polynom.hs
@@ -4,3 +4,9 @@ type Polynom = [Double]
 
 cmult :: Polynom -> Double -> Polynom
 cmult poly c = map (*c) poly
+
+eval :: Polynom -> Double -> Double
+eval poly x = foldr ((+).(x*)) 0 poly
+-- these are functionally the same. The latter one shows the horner scheme more clearly while the former is more concise
+evalLambda :: Polynom -> Double -> Double
+evalLambda poly x = foldr (\aN acc -> aN + (x * acc)) 0 poly
